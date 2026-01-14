ec876dc7d09be08f2737b6d127fc9587cd8fb76b
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-01-14 21:36:04 +0200

    added StackCard component

---
+++ frontend/src/components/StackCard.tsx
@@ -0,0 +1,31 @@
+type Props = {
+    icon: string;
+    isFullWidth?: boolean;
+    techIcons: string[];
+    title: string;
+};
+
+const StackCard = ({ icon, isFullWidth = false, techIcons, title }: Props) => {
+    return (
+        <div
+            className={`
+            card ${isFullWidth ? "col-span-3" : ""} flex flex-col p-6 
+        `}
+        >
+            <div className="dark:text-white flex gap-3 items-center text-slate-900">
+                <span className="material-symbols-outlined select-none">
+                    {icon}
+                </span>
+                <span className="font-bold text-lg">{title}</span>
+            </div>
+            <hr className="border-light dark:border-dark mt-3 mb-6" />
+            <div className="flex flex-wrap gap-8">
+                {techIcons.map((url, idx) => (
+                    <img className="h-11.5 w-11.5" key={idx} src={url} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default StackCard;
