d7b74ced992e38aaaa4177a3271c5245d57f63dc
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-01-16 17:40:42 +0200

    fixed TechStack icons

--- frontend/src/components/StackCard.tsx
+++ frontend/src/components/StackCard.tsx
@@ -8,9 +8,9 @@ type Props = {
 const StackCard = ({ icon, isFullWidth = false, techIcons, title }: Props) => {
     return (
         <div
-            className={`
-            card ${isFullWidth ? "col-span-3" : ""} flex flex-col p-6 
-        `}
+            className={`card ${
+                isFullWidth ? "col-span-3" : ""
+            } flex flex-col p-6`}
         >
             <div className="dark:text-white flex gap-3 items-center text-slate-900">
                 <span className="material-symbols-outlined select-none">
@@ -19,9 +19,9 @@ const StackCard = ({ icon, isFullWidth = false, techIcons, title }: Props) => {
                 <span className="font-bold text-lg">{title}</span>
             </div>
             <hr className="border-light dark:border-dark mt-3 mb-6" />
-            <div className="flex flex-wrap gap-8">
+            <div className="flex flex-wrap gap-4">
                 {techIcons.map((url, idx) => (
-                    <img className="h-11.5 w-11.5" key={idx} src={url} />
+                    <img className="h-14.5 w-14.5" key={idx} src={url} />
                 ))}
             </div>
         </div>
--- frontend/src/components/TechStack.tsx
+++ frontend/src/components/TechStack.tsx
@@ -1,3 +1,4 @@
+import robotFrameworkLogo from "../assets/images/robot-framework.svg";
 import StackCard from "./StackCard";
 
 const TechStack = () => {
@@ -13,47 +14,47 @@ const TechStack = () => {
                 <StackCard
                     icon="desktop_windows"
                     techIcons={[
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg",
                         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
                     ]}
                     title="Frontend"
                 />
                 <StackCard
                     icon="dns"
                     techIcons={[
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original-wordmark.svg",
                         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg"
                     ]}
                     title="Backend"
                 />
                 <StackCard
-                    icon="web"
+                    icon="language"
                     techIcons={[
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
                         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg"
                     ]}
                     title="Web Dev"
                 />
                 <StackCard
-                    icon="build_circle"
+                    icon="bug_report"
                     techIcons={[
+                        robotFrameworkLogo,
                         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cypressio/cypressio-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/junit/junit-original.svg"
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/junit/junit-original-wordmark.svg"
                     ]}
                     title="Testing & Automation"
                 />
                 <StackCard
                     icon="code"
                     techIcons={[
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg",
                         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg",
                         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"
                     ]}
                     title="Programming Languages"
@@ -61,21 +62,22 @@ const TechStack = () => {
                 <StackCard
                     icon="database"
                     techIcons={[
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original-wordmark.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg"
                     ]}
                     title="Databases"
                 />
                 <StackCard
-                    icon="cloud_done"
+                    icon="cloud"
                     isFullWidth={true}
                     techIcons={[
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/githubactions/githubactions-original.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg",
-                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg"
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original-wordmark.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/githubactions/githubactions-original-wordmark.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original-wordmark.svg",
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg"
                     ]}
                     title="Version Control, CI/CD & Cloud Services"
                 />
