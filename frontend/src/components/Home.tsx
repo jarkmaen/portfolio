843c704a61d24ae13996e643e11497fa9382f946
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-01-21 22:25:43 +0200

    started Timeline component

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "./Hero";
 import TechStack from "./TechStack";
+import Timeline from "./Timeline";
 
 const Home = () => {
     return (
         <div className="flex flex-col gap-9">
             <Hero />
             <TechStack />
+            <Timeline />
         </div>
     );
 };
---
+++ frontend/src/components/Timeline.tsx
@@ -0,0 +1,14 @@
+const Timeline = () => {
+    return (
+        <>
+            <div className="dark:text-white flex gap-3 items-center text-slate-900">
+                <span className="material-symbols-outlined select-none text-3xl!">
+                    timeline
+                </span>
+                <h4 className="font-bold text-3xl">My Developer Journey</h4>
+            </div>
+        </>
+    );
+};
+
+export default Timeline;
